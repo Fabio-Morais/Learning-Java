cc2da221e9d9d60bfb07eda7b34b325053576923
Fábio Morais <fabiouds@hotmail.com>
2019-08-03 00:43:11 +0100

    update

--- Lesson_9/Tic-Tac-Toe with AI/Tic-Tac-Toe with AI/task/src/tictactoe/GameScreen.java
+++ Lesson_9/Tic-Tac-Toe with AI/Tic-Tac-Toe with AI/task/src/tictactoe/GameScreen.java
@@ -1,12 +1,13 @@
 package tictactoe;
 
-import java.util.ArrayList;
-
 public class GameScreen {
-    private ArrayList <Character> symbols;
-
+    private char[][] symbols;
+    private int numX;
+    private int numY;
     public GameScreen() {
-        this.symbols = new ArrayList<Character>();
+        this.symbols = new char[3][3];
+        numX=0;
+        numY=0;
     }
 
     public void setSymbols(String cells) {
@@ -14,31 +15,87 @@ public class GameScreen {
         if(cells.length() != 11)
             System.out.println("error");
         else{
+            int c=0;
+            int r=0;
             for(int i=1; i<cells.length()-1; i++){
-                this.symbols.add(Character.toUpperCase(cells.charAt(i)));
+
+                if(i==4 || i==7)
+                {
+                    c++;
+                    r=0;
+                }
+                this.symbols[c][r] = Character.toUpperCase(cells.charAt(i));
+                if(this.symbols[c][r] == "X".charAt(0))
+                    numX++;
+                else if(this.symbols[c][r] == "O".charAt(0))
+                    numY++;
+                r++;
             }
         }
     }
 
+    public boolean isEmpty(int x, int y){
+        int x1=Math.abs(x-1);
+        int y1=Math.abs(y-3);
+
+        return this.symbols[y1][x1]==" ".charAt(0);
+
+    }
+
     public void printGame(){
-       if(this.symbols.size() == 9) {
+
            for (int i = 0; i < 5; i++) {
                if (i == 0 || i == 4)
                    System.out.println("---------");
                else {
                    System.out.print("| ");
-                   for (int j = ((i-1) * 3); j < (3 + ((i-1) * 3)); j++) {
-                       System.out.print(symbols.get(j) + " ");
+                   for (int j = 0; j < 3; j++) {
+                       System.out.print(symbols[i-1][j] + " ");
                    }
                    System.out.print("|\n");
                }
 
            }
-       }
+
 
     }
 
-    public void gameState(){
+    public void setCoordSymb(int x, int y){
+        int x1=Math.abs(x-1);
+        int y1=Math.abs(y-3);
+        this.symbols[y1][x1]="X".charAt(0);
+    }
 
+    public void gameState(){
+        boolean xWins=false;
+        boolean oWins=false;
+        String winV, winH, winD;
+        /*Diagonal*/
+        winD=""+this.symbols[0][0]+""+this.symbols[1][1]+""+this.symbols[2][2]+ ""+this.symbols[0][2]+""+this.symbols[1][1]+""+this.symbols[2][0];
+
+        for(int i=0; i<3; i++){
+                /*vertical*/
+            winV=""+this.symbols[i][0]+""+this.symbols[i][1]+""+this.symbols[i][2];
+                /*horizontal*/
+            winH=""+this.symbols[0][i]+""+this.symbols[1][i]+""+this.symbols[2][i];
+
+            if(winV.equals("XXX") || winH.equals("XXX") || winD.substring(0,3).equals("XXX") || winD.substring(3,6).equals("XXX"))
+                xWins=true;
+            else if(winV.equals("OOO") || winH.equals("OOO") || winD.substring(0,3).equals("OOO") || winD.substring(3,6).equals("OOO"))
+                oWins=true;
+
+        }
+
+        /* Print game state */
+        if(xWins && oWins || (Math.abs(numX-numY)>1))
+            System.out.println("Impossible");
+        else if(xWins)
+            System.out.println("X wins");
+        else if(oWins)
+            System.out.println("O wins");
+        else if((numY+numX) != 9)
+            System.out.println("Game not finished");
+        else
+            System.out.println("Draw");
     }
 }
--- Lesson_9/Tic-Tac-Toe with AI/Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Lesson_9/Tic-Tac-Toe with AI/Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -4,12 +4,41 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Enter cells: ");
-        String cells= scanner.nextLine();
+        String cells = scanner.nextLine();
         GameScreen tikTak = new GameScreen();
         tikTak.setSymbols(cells);
         tikTak.printGame();
+
+        String aux;
+        int x = 4;
+        int y = 4;
+
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            aux = scanner.nextLine();
+
+            if (!Character.isDigit(aux.charAt(0)) || !Character.isDigit(aux.charAt(2)))
+                System.out.println("You should enter numbers!");
+            else {
+                x = Character.getNumericValue(aux.charAt(0));
+                y = Character.getNumericValue(aux.charAt(2));
+                if ((x >= 1 && x <= 3) && (y >= 1 && y <= 3) && tikTak.isEmpty(x,y))
+                    break;
+                else if((x >= 1 && x <= 3) && (y >= 1 && y <= 3) && !tikTak.isEmpty(x,y))
+                    System.out.println("This cell is occupied! Choose another one!");
+                else
+                    System.out.println("Coordinates should be from 1 to 3!");
+            }
+
+        }
+
+
+        tikTak.setCoordSymb(x, y);
+        tikTak.printGame();
+        //  tikTak.gameState();
     }
 
 }
